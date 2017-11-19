0b2aa7ab3e18c0a779a840162e1edddb1465856b
terribledevice <longlost@gmail.com>
2017-11-19 11:34:40 -0500

    fixed env variables

--- postinstall.sh
+++ postinstall.sh
@@ -2,8 +2,8 @@
 
 IP_ADDR=$(ip addr | grep -E "BROADCAST," | cut -c 3-9)
 
-touch /etc/sysconfig/network-scripts/ifcfg-$IP_ADDR
-sed -i -e 's/ONBOOT=no/ONBOOT=yes'/etc/sysconfig/network-scripts/ifcfg-$IP_ADDR
+touch /etc/sysconfig/network-scripts/ifcfg-$(echo $IP_ADDR)
+sed -i -e 's/ONBOOT=no/ONBOOT=yes'/etc/sysconfig/network-scripts/ifcfg-$(echo $IP_ADDR)
 ifup $IP_ADDR
 
 yum update
