89d78366b34c5554ebda5d1c50a9b6ad02d3acbc
terribledevice <longlost@gmail.com>
2017-11-19 13:00:26 -0500

    commented on what script does

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# sets variable based on network interface
 IP_ADDR=$(ip addr | grep -E "BROADCAST," | cut -c 4-9)
 
-touch /etc/sysconfig/network-scripts/ifcfg-$(echo $IP_ADDR)
+# edits network interface script, enables network on next boot
 sed -i -e 's/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/ifcfg-$(echo $IP_ADDR)
+
+# enables network interface for current login
 ifup $IP_ADDR
 
-yum update
-yum install nmap htop vim git -y
+# updates yum, installs a couple of applications
+yum update && yum install nmap vim git -y
