7c6900cdf18f883a1aef2c58c9f27cc8d41937f3
terribledevice <longlost@gmail.com>
2017-11-19 11:05:38 -0500

    updated network command

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@ IP_ADDR=$(ip addr | grep -E "BROADCAST," | cut -c 3-9)
 
 touch /etc/sysconfig/network-scripts/ifcfg-$IP_ADDR
 sed -i -e 's/ONBOOT=no/ONBOOT=yes'/etc/sysconfig/network-scripts/ifcfg-$IP_ADDR
-systemctl restart network.service
+ifup $IP_ADDR
 
 yum update
 yum install nmap htop vim git -y
